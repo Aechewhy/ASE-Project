4b1052c7954c72b96a3e390022aaa65840ab06fd
bakarekt <nvdung1020@gmail.com>
2024-12-17 10:17:57 +0700

    Xong tính năng search

--- src/routes/certificateFacilityRoutes.js
+++ src/routes/certificateFacilityRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const certificateFacilityController = require("../app/controllers/certificateFacilityController");
 
+router.get("/create", certificateFacilityController.create);
+router.post("/store", certificateFacilityController.store);
+router.get("/:id/edit", certificateFacilityController.edit);
+router.put("/:id", certificateFacilityController.update);
+router.get("/:id", certificateFacilityController.detail);
 router.get("/", certificateFacilityController.certificateFacility);
 
 module.exports = router;
