cba1d21e4b6c4cacb3f118993d2ecff58f2ffc70
Aechewhy <harryhuy2472004@gmail.com>
2024-12-23 16:13:41 +0700

    slaughterhouse table functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,36 +1,27 @@
+const certificateFacilityRouter = require("./certificateFacilityRoutes");
+const certificateRouter = require("./certificateRoutes");
+const loginRouter = require("./loginRoutes");
+const processingFacilityRouter = require("./processingFacilityRoutes");
+const slaughterhouseRouter = require("./slaughterhouseRoutes");
+const raisingFacilityRouter = require("./raisingFacilityRoutes");
 const searchRouter = require("./searchRoutes");
 const siteRouter = require("./site");
 const userRouter = require("./userRoutes");
-const loginRouter = require("./loginRoutes");
-const raisingFacilityRouter = require("./raisingFacilityRoutes");
-const certificateRouter = require("./certificateRoutes");
-const certificateFacilityRouter = require("./certificateFacilityRoutes");
 const wasteTreatmentFacilityRouter = require("./wasteTreatmentFacilityRoutes");
 const wasteTreatmentProductRouter = require("./wasteTreatmentProductRoutes");
-const processingFacilityRouter = require("./processingFacilityRoutes");
-
 
 function route(app) {
-
-  app.use("/processingFacility", processingFacilityRouter);
-  
-  app.use("/wasteTreatmentProduct", wasteTreatmentProductRouter);
-
-  app.use("/wasteTreatmentFacility", wasteTreatmentFacilityRouter);
-
-  app.use("/certificate", certificateRouter);
-
-  app.use("/certificateFacility", certificateFacilityRouter);
-
-  app.use("/raisingFacility", raisingFacilityRouter);
-
-  app.use("/login", loginRouter);
-
-  app.use("/user", userRouter);
-
-  app.use("/search", searchRouter);
-
-  app.use("/", siteRouter);
+    app.use("/", siteRouter);
+    app.use("/certificate", certificateRouter);
+    app.use("/certificateFacility", certificateFacilityRouter);
+    app.use("/login", loginRouter);
+    app.use("/processingFacility", processingFacilityRouter);
+    app.use("/raisingFacility", raisingFacilityRouter);
+    app.use("/search", searchRouter);
+    app.use("/slaughterhouse", slaughterhouseRouter);
+    app.use("/user", userRouter);
+    app.use("/wasteTreatmentFacility", wasteTreatmentFacilityRouter);
+    app.use("/wasteTreatmentProduct", wasteTreatmentProductRouter);
 }
 
 module.exports = route;
